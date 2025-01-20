f5b6516ac76ffcc8eb229279310a34670861f35d
Bankole Abiodun <110631073+meowbanky@users.noreply.github.com>
2025-01-20 10:29:53 +0000

    add download link for ios mobile

--- src/components/Home.js
+++ src/components/Home.js
@@ -353,7 +353,7 @@ const Home = () => {
                             <FaArrowRight className="w-5 h-5"/>
                             <span>Download for Android</span>
                         </a>
-                        <a href="#" className="flex items-center gap-4 text-blue-600 hover:text-blue-700">
+                        <a href="https://oouth-coop.web.app/" className="flex items-center gap-4 text-blue-600 hover:text-blue-700">
                             <FaArrowRight className="w-5 h-5"/>
                             <span>Download for iOS</span>
                         </a>
